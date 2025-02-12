c803e52b308dcc4133151676a71ef1e8a7ad13d5
elliotharding <elliot.one.harding@gmail.com>
2025-02-12 08:51:16 +0000

    Commit 3

--- angular2/hello-world/src/app/app.component.ts
+++ angular2/hello-world/src/app/app.component.ts
@@ -8,31 +8,17 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 
-/**/
 @Component({
   selector: 'app-hello-world',
-  template: `
-    <p>app-hello-world</p>
-  `
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 
 export class HelloWorldComponent {
   name = 'World';
-  showGreeting = true; // Add a property to control visibility
-  names = ['Alice', 'Bob', 'Charlie'];
 }
 
 export class AppComponent {
-  title = 'hello-world';
-}
-
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
-
-@Directive({
-  selector: '[appHighlight]' // Attribute selector
-})
-export class HighlightDirective {
-  constructor(private el: ElementRef, private renderer: Renderer2) {
-    this.renderer.setStyle(this.el.nativeElement, 'background-color', 'yellow');
-  }
+  title = 'hello world';
 }
