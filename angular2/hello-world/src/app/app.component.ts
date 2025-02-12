28b1786a57c795159c28ce69bf4bde92c05bc77e
elliotharding <elliot.one.harding@gmail.com>
2025-02-12 10:38:56 +0000

    COmmit 4

--- angular2/hello-world/src/app/app.component.ts
+++ angular2/hello-world/src/app/app.component.ts
@@ -2,23 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-
-@Component({
   selector: 'app-hello-world',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
-export class HelloWorldComponent {
-  name = 'World';
-}
-
-export class AppComponent {
-  title = 'hello world';
-}
