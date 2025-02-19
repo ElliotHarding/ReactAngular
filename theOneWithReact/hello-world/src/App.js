cad916501b6956601ac4af6fdc9392006c66ae46
elliotharding <elliot.one.harding@gmail.com>
2025-02-19 16:39:47 +0000

    TheOneWithReact has a dotnet helloworld application

--- theOneWithReact/hello-world/src/App.js
+++ theOneWithReact/hello-world/src/App.js
@@ -4,9 +4,17 @@ import './App.css';
 import React from 'react';
 
 function App() {
+
+	const items = ["Apple", "Banana", "Orange"];
+
   return (
     <div className="App">
       <h1>Hello, World!</h1>
+      <ul>
+		  {items.map((item, index) => (
+		    <li key={index}>{item}</li>
+		  ))}
+    	</ul>
     </div>
   );
 }
