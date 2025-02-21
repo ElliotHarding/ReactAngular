ce9df66864e1a07ebb3800547bea64e4615b13f6
elliotharding <elliot.one.harding@gmail.com>
2025-02-21 12:58:23 +0000

    Products js and css

--- theOneWithReact/react-hello-world/src/Products.js
+++ theOneWithReact/react-hello-world/src/Products.js
@@ -7,6 +7,8 @@ function ProductList() {
   var [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [newProductName, setNewProductName] = useState('');
+  const [newProductPrice, setNewProductPrice] = useState('');
   
   const fetchData = async () => {
       setLoading(true);
@@ -59,18 +61,65 @@ function ProductList() {
    useEffect(() => {
 		fetchData();
    }, []);
+   
+  //Add product
+  const handleAddProduct = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/Products/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: newProductName,
+          price: parseFloat(newProductPrice),
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const newProduct = await response.json();
+      setProducts((prevProducts) => [...prevProducts, newProduct]);
+      setNewProductName('');
+      setNewProductPrice(''); //clear form.      
+    } catch (err) {
+      setError(err);
+    }
+  };
 
   return (
   	<div className="ProductList">
-  	<h2>Products (Dotnet)</h2>
-  	<ul>
-        {products && products.map((product) => (
-          <li key={product.id}>
-            {product.name} - ${product.price}
-            <a>   </a>
-            <button onClick={() => handleDelete(product.id)}>X</button>
-          </li>
-        ))}
+  	
+	  	<h2>Products (Dotnet) <
+	  	button onClick={() => fetchData()}>Fetch Data!</button>
+	  	</h2>
+	  	
+	  	<div>
+		    <input
+		      type="text"
+		      placeholder="Product Name"
+		      value={newProductName}
+		      onChange={(e) => setNewProductName(e.target.value)}
+		    />
+		    <input
+		      type="number"
+		      placeholder="Product Price"
+		      value={newProductPrice}
+		      onChange={(e) => setNewProductPrice(e.target.value)}
+		    />
+		    <button onClick={handleAddProduct}>Add Product</button>
+      	</div>
+	  	
+	  	<ul>
+		    {products && products.map((product) => (
+		      <li key={product.id}>
+		        {product.name} - ${product.price}
+		        <a>   </a>
+		        <button onClick={() => handleDelete(product.id)}>X</button>
+		      </li>
+		    ))}
       </ul>
   	</div>
   );
