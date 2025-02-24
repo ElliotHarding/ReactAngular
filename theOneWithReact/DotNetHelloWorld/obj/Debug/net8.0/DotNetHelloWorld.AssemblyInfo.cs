f2f73db87de42e23fc494628a0f08c97887f632e
elliotharding <elliot.one.harding@gmail.com>
2025-02-24 16:26:12 +0000

    Starting with doker containers

--- theOneWithReact/DotNetHelloWorld/obj/Debug/net8.0/DotNetHelloWorld.AssemblyInfo.cs
+++ theOneWithReact/DotNetHelloWorld/obj/Debug/net8.0/DotNetHelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetHelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38f5c3496bedafed24d63a402866a5f5875fb510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d18e8c8a4d07c8633aaebf242eb62338098c6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetHelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetHelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
